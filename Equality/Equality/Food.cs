9cd256a43e2dd0fc0a6c0c6397d62b163e09c985
michalurbanski <michal_urbanski@windowslive.com>
2015-07-12 12:53:47 +0000

    Implements equality for base class

--- Equality/Equality/Food.cs
+++ Equality/Equality/Food.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace Equality
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>There should be confidence that developers using this class will not be confused
+    /// because of custom equality comparison implemented. 
+    /// Implementing IEquatable interface does not provide any values here</remarks>
     internal class Food
     {
         private readonly string _name;
@@ -23,6 +29,34 @@ namespace Equality
         public override string ToString()
         {
             return _name; 
+        }
+
+        public static bool operator ==(Food x, Food y)
+        {
+            return object.Equals(x, y);
+        }
+
+        public static bool operator !=(Food x, Food y)
+        {
+            return !object.Equals(x, y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (ReferenceEquals(obj, this))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+
+            Food rhs = obj as Food; // this cast is safe because check for null was earlier
+            return _name == rhs.Name && _group == rhs.Group;
+        }
+
+        public override int GetHashCode()
+        {
+            return _name.GetHashCode() ^ _group.GetHashCode(); 
         }
     }
 }
