3d377886bc42010e25dcb6f057569bf37c06070f
michalurbanski <michal_urbanski@windowslive.com>
2015-07-12 10:57:06 +0000

    Comments for reason of implementation

--- Equality/Equality/Program.cs
+++ Equality/Equality/Program.cs
@@ -17,9 +17,22 @@ namespace Equality
         /// <summary>
         /// Implements value types equality using structures
         /// </summary>
+        /// <remarks>
+        /// Own equality implementation for value type should be made in order to: 
+        /// 1. Use == operator
+        /// 2. Performance - avoid boxing and unboxing and also using reflection
+        /// 3. Change meaning of Equals(), e.g. not all fields may be checked
+        /// 
+        /// Steps required to implement equality (best practice is to implement all 3): 
+        /// 1. Override object.Equals() -> avoid reflection
+        /// 2. Implement IEquatable<FoodItem> -> avoid boxing, give type safety
+        /// 3. Allow using == operator 
+        /// 4. Implement != operator
+        /// 5. Implement object.GetHashCode() - required by Equals()
+        /// </remarks>
         internal static void ValueTypesEquality()
         {
-
+            
         }
     }
 }
